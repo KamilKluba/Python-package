b656e6dfb3962b096878d3f76b33fffbd35a6b9f
KamilKluba <226016@student.pwr.edu.pl>
2019-05-20 02:19:53 +0200

    Fourth try.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license='MIT',
     description='A DPP python package',
     long_description=open('README.md').read(),
-    install_requires=['string'],
+    install_requires=[],
     url='https://github.com/KamilKluba/Python-package',
     author='KamilKluba',
     author_email='226016@student.pwr.edu.pl'
