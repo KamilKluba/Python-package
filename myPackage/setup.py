4a2ef312c4223691cea31fe833bd2af9e51dd8bd
KamilKluba <226016@student.pwr.edu.pl>
2019-05-20 02:14:28 +0200

    Second try.

--- myPackage/setup.py
+++ myPackage/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='DPP10_1',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A DPP python package',
     long_description=open('README.md').read(),
     install_requires=['string'],
-    url='https://github.com/KamilKluba/Python-package.git',
+    url='https://github.com/KamilKluba/Python-package',
     author='KamilKluba',
     author_email='226016@student.pwr.edu.pl'
 )
